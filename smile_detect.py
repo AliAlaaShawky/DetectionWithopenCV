48066c2e7597276d74d8352e7e5d5f6be083cac0
ali alaa <86577146+alialaabos@users.noreply.github.com>
2022-12-22 17:55:43 +0200

    Add files via upload

---
+++ smile_detect.py
@@ -0,0 +1,35 @@
+
+import cv2
+cam=cv2.VideoCapture("C:/Users/10/Desktop/New folder (2)/ali.mp4")
+detector=cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye=cv2.CascadeClassifier('haarcascade_eye.xml') 
+smile=cv2.CascadeClassifier('haarcascade_smile.xml')
+while (True):
+    ret,img=cam.read()
+    #img=cv2.resize(img,(600,300))
+    gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    ####---------------face---------------###
+    faces=detector.detectMultiScale(gray,1.3,5)
+    for(x,y,w,h) in faces:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+        ###------eye detection-----###
+        face_only=img[y:y+h,x:x+w]
+        eyes=eye.detectMultiScale(face_only,1.3,5)
+        for(ex,ey,ew,eh) in eyes:
+             #cv2.rectangle(face_only,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+            eyex=int((ex-(0.25*ex))+(ew/2))
+            eyey=int((ey)+(eh))
+            cv2.putText(face_only,"x",(eyex,eyey),cv2.FONT_HERSHEY_COMPLEX,4,(255,0,0),4)
+           ###---------smile detect-----------###
+        smiles=smile.detectMultiScale(face_only,1.3,20)
+        for(sx,sy,sw,sh) in smiles:
+             cv2.rectangle(face_only,(sx,sy),(sx+sw,sy+sh),(0,0,255),2)
+        cv2.imshow('frame',img)
+    
+
+    if cv2.waitKey(20) &0xFF ==ord('q'):
+        break
+    
+        
+cam.release()
+cv2.destroyAllWindows()
